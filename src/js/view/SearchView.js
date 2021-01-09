e8236ef418d4bb6cdfc1f0a2d3a986f0c45a9285
Banzragch <cbanzragch1@gmail.com>
2021-01-09 23:08:56 +0800

    DONE ALL

--- src/js/view/SearchView.js
+++ src/js/view/SearchView.js
@@ -1,9 +1,7 @@
-// searchView deer delgetstei holbootoi ymnud bichne
 import { elements } from "./base";
-// Private function 
+
 const renderRecipe = recipe => {
-    console.log(recipe.title);
-    const markup = ` 
+  const markup = `
     <li>
         <a class="results__link" href="#${recipe.recipe_id}">
             <figure class="results__fig">
@@ -15,55 +13,60 @@ const renderRecipe = recipe => {
             </div>
         </a>
     </li>
-    `;
-// ul рүүгээ нэмнэ
-elements.searchResultList.insertAdjacentHTML("beforeend", markup);
+  `;
+  // ul рүүгээ нэмнэ
+  elements.searchResultList.insertAdjacentHTML("beforeend", markup);
 };
 export const clearSearchQuery = () => {
-  elements.searchInput.value = ""; 
-  elements.pageButtons.innerHTML = ""; 
+  elements.searchInput.value = "";
 };
 
 export const clearSearchResult = () => {
   elements.searchResultList.innerHTML = "";
+  elements.pageButtons.innerHTML = "";
 };
+
 export const getInput = () => elements.searchInput.value;
 
 export const renderRecipes = (recipes, currentPage = 1, resPerPage = 10) => {
-// Хайлтын үр дүнг хуудаслаж үзүүлэх
-    const start = (currentPage-1) * resPerPage;
-    const end = currentPage * resPerPage;
+  // Хайлтын үр дүнг хуудаслаж үзүүлэх
+  //page  = 2, start = 10, end = 20
+  const start = (currentPage - 1) * resPerPage;
+  const end = currentPage * resPerPage;
 
-  recipes.slice(start,end).forEach(renderRecipe);
-//   Хуудаслалтын товчуудыг гаргаж ирэх
-  const totalPages = Math.cell(recipe.length / resPerPage);
- renderButtons (currentPage , totalPage);
+  recipes.slice(start, end).forEach(renderRecipe);
+
+  // Хуудаслалтын товчуудыг гаргаж ирэх
+  const totalPages = Math.ceil(recipes.length / resPerPage);
+  renderButtons(currentPage, totalPages);
 };
+
+// type ===> 'prev', 'next'
 const createButton = (
-    pege,
-    type,
-    direction
-) => `
-<button class="btn-inline results__btn--${type}" data-goto=${psge}>
-<span>Хуудас ${page}</span> 
-<svg class="search__icon">
+  page,
+  type,
+  direction
+) => `<button class="btn-inline results__btn--${type}" data-goto=${page}>
+  <span>Хуудас ${page}</span>
+  <svg class="search__icon">
     <use href="img/icons.svg#icon-triangle-${direction}"></use>
-</svg>
+  </svg>
+  </button>`;
 
-</button>
-`;
+const renderButtons = (currentPage, totalPages) => {
+  let buttonHtml;
 
-const renderButtons = (currentPage, totalPage) => {
-    let button;
-    if(currentPage === 1 && totalPage > 1) {
-     button = createButton(2, "next" , 'right');
-    } else if (currentPage < totalPages) {
-        button = createButton(currentPage-1, "prev", " left");
-        button += createButton(currentPage+1, "next", "right");
-    } else if (currentPage === totalPage) {
-     button = createButton(currentPage-1, "prev", "left");
-    }
+  if (currentPage === 1 && totalPages > 1) {
+    // 1-р хуудсан дээр байна, 2-р хуудас гэдэг товчийг гарга
+    buttonHtml = createButton(2, "next", "right");
+  } else if (currentPage < totalPages) {
+    // Өмнөх болон дараачийн хуудас руу шилжих товчуудыг үзүүл
+    buttonHtml = createButton(currentPage - 1, "prev", "left");
+    buttonHtml += createButton(currentPage + 1, "next", "right");
+  } else if (currentPage === totalPages) {
+    // Хамгийн сүүлийн хуудас дээр байна. Өмнөх рүү шилжүүлэх товчийг л үзүүлнэ.
+    buttonHtml = createButton(currentPage - 1, "prev", "left");
+  }
 
-    elements.pageButtons.insertAdjacentHTML('afterbegin ', button);
-    
+  elements.pageButtons.insertAdjacentHTML("afterbegin", buttonHtml);
 };
--- src/js/view/base.js
+++ src/js/view/base.js
@@ -1,27 +1,33 @@
-// Vvnd eronhii CLASS uudiin neriig hadgalna 
 export const elements = {
-    searchForm: document.querySelector(".search"),
-    searchInput: document.querySelector(".search__field"),
-    searchResultDiv: document.querySelector(".results"),
-    searchResultList: document.querySelector(".results__list"),
-    pageButtons: document.querySelector(".results__pages")
-  };
-export const elementStrings = () => {
-    loader: 'loader'
-}; 
-export const clearLoader = () => {
-    const loader = document.querySelector(`.${elementStrings.loader}`);
-
-    if(loader) loader.parentElement.removeChild(loader);
+  searchForm: document.querySelector(".search"),
+  searchInput: document.querySelector(".search__field"),
+  searchResultDiv: document.querySelector(".results"),
+  searchResultList: document.querySelector(".results__list"),
+  pageButtons: document.querySelector(".results__pages"),
+  recipeDiv: document.querySelector(".recipe"),
+  shoppingList: document.querySelector(".shopping__list"),
+  likesMenu: document.querySelector(".likes__field"),
+  likesList: document.querySelector(".likes__list")
 };
-export const renderLoader =  parent => {
-    const loader = `
-    <div class="${elementStrings.loader}">''
-        <svg>
-        <use href="img/icons.svg#icon-cw"</use>
-        </svg>
-    </div>
-     `;
 
-    parent.insertAdjacentHTML('afterbegin', loader);
+export const elementStrings = {
+  loader: "loader"
+};
+
+export const clearLoader = () => {
+  const loader = document.querySelector(`.${elementStrings.loader}`);
+
+  if (loader) loader.parentElement.removeChild(loader);
+};
+
+export const renderLoader = parent => {
+  const loader = `
+        <div class="${elementStrings.loader}">
+            <svg>
+                <use href="img/icons.svg#icon-cw"</use>
+            </svg>
+        </div>
+    `;
+
+  parent.insertAdjacentHTML("afterbegin", loader);
 };
